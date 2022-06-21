f66f6c9e923588782c2f197faeb51a40657ebe48
JohnPonciano <jonathan.ponciano@hotmail.com>
2022-06-21 19:10:02 -0300

    Added Brazilian lofi more covers

--- data.js
+++ data.js
@@ -1,8 +1,26 @@
 let songs = [
     {
+        name: 'Azul da Cor do Mar',
+        path: 'songs/azul-da-cor-do-mar-lofi-remix.mp3',
+        artist: 'GabrielMzero',
+        cover: 'images/cover2.png'
+    },
+    {
+        name: 'Ela Partiu',
+        path: 'songs/ela-partiu-lofi-remix.mp3',
+        artist: 'Tim Maia',
+        cover: 'images/cover3.png'
+    },
+    {
+        name: 'Ela mora no meu peito',
+        path: 'songs/ela-mora-no-meu-peito-ofi-remix.mp3',
+        artist: 'GabrielMzero - Original: Jorge Ben e Toquinho - Carolina Bela',
+        cover: 'images/cover4.png'
+    },
+    {
         name: 'Sailor Moon 2',
         path: 'songs/sailormoon2.mp3',
         artist: 'Aesthetic2',
         cover: 'images/capa.jpg'
-    }
+    },
 ]
